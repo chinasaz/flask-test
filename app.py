f4449e6d39d0d7ef5150a3b075997501442bdd4b
Chinasa Zebe <120458152+chinasaz@users.noreply.github.com>
2023-08-16 08:50:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, Welcome to Flask!"
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_numbers():
