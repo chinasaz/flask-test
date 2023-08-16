e40130bb447ff014d7bfb4aa6b94c6a5f94b9124
Chinasa Zebe <120458152+chinasaz@users.noreply.github.com>
2023-08-16 08:54:41 +0100

    Update app.py
    added a phrase

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Welcome to Flask!"
+    return "Hello, Welcome to Flask App!"
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_numbers():
